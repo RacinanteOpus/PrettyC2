abbc4a907f244940aa47fbcc29b1a041b3c1cc30
RacinanteOpus <racinante@racinante.com>
2020-10-26 12:04:22 -0500

    Update tables.js

--- tables.js
+++ tables.js
@@ -7,7 +7,7 @@ function myToggle() {
   var x = document.getElementsByClassName("hideme");
   for (var i = 0; i < x.length; i++) {
   if (x[i].style.display === "none") {
-    	x[i].style.display = "";
+    	x[i].style.display = "table-cell";
   	} else {
     	x[i].style.display = "none";
 	}
