bd66d363a34f59fa939655ee541fa0c54a9e2f11
RacinanteOpus <racinante@racinante.com>
2020-10-30 00:42:30 -0500

    Update tables.js

--- tables.js
+++ tables.js
@@ -6,7 +6,7 @@ function numberWithCommas(x,y) {
 function myToggle() {
   var x = document.getElementsByClassName("hideme");
   try {
-	  if (x[7].style.display === "table-cell") {
+	  if (x[9].style.display === "table-cell") {
   		for (var i = 0; i < x.length; i++) {
 	    		x[i].style.display = "none";
 	  	} 
